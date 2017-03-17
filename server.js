b6037e223b19171c95d88ad815b24f40370eea8e
Nikolaos Maniatis <maniatis@museotechniki.com>
2017-03-17 01:04:18 +0000

    hello world

---
+++ server.js
@@ -0,0 +1,20 @@
+//call .env variables
+const dotenv = require('dotenv');
+dotenv.load();
+
+//init modules
+const Twit = require('twit');
+const request = require('request');
+
+//twitter authorisaton
+var T = new Twit({
+  consumer_key: process.env.CONSUMER_KEY,
+  consumer_secret: process.env.CONSUMER_SECRET,
+  access_token: process.env.ACCESS_TOKEN,
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET,
+})
+
+//  tweet 'hello world!' 
+T.post('statuses/update', { status: 'hello world!' }, function(err, data, response) {
+  console.log(data)
+})
